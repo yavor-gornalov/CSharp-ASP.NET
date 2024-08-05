60c284a9c9bf2bee89fa35655a90ffb5a41ea7b8
Yavor Gornalov <y.gornalov@gmail.com>
2024-08-05 14:14:19 +0300

    Seminar All view and model --created

--- SeminarHub/SeminarHub/Controllers/SeminarController.cs
+++ SeminarHub/SeminarHub/Controllers/SeminarController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SeminarHub.Services;
+using SeminarHub.Services.Contracts;
 
-namespace SeminarHub.Controllers
+namespace SeminarHub.Controllers;
+
+public class SeminarController : BaseController
 {
-	public class SeminarController : BaseController
+	private readonly ISeminarService seminarService;
+
+	public SeminarController(ISeminarService _service)
 	{
-		public IActionResult All()
-		{
-			return View();
-		}
+		seminarService = _service;
+	}
+
+	public async Task<IActionResult> All()
+	{
+		var model = await seminarService.GetAllSeminarsAsync();
+
+		return View(model);
+	}
+
+	public async Task<IActionResult> Add()
+	{
+		return View();
 	}
 }
---
+++ SeminarHub/SeminarHub/Models/SeminarAllViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SeminarHub.Data.Common;
+using SeminarHub.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeminarHub.Models;
+
+public class SeminarAllViewModel
+{
+	public int Id { get; set; }
+
+	public string Topic { get; set; } = null!;
+
+	public string Lecturer { get; set; } = null!;
+
+	public string Organizer { get; set; } = null!;
+
+	public string DateAndTime { get; set; } = null!;
+
+	public string Category { get; set; } = null!;
+
+}
