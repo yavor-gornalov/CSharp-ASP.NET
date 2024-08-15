70154daf25c86f1c6e0a634c281b73f6c8596d67
Yavor Gornalov <y.gornalov@gmail.com>
2024-08-15 15:06:53 +0300

    Custom Error Pages --uploaded

--- HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -6,25 +6,36 @@ using System.Diagnostics;
 namespace HouseRentingSystem.Controllers
 {
     public class HomeController : Controller
-	{
-		private readonly IHouseService _houseService;
+    {
+        private readonly IHouseService _houseService;
 
-		public HomeController(IHouseService houseService)
-		{
-			_houseService = houseService;
-		}
+        public HomeController(IHouseService houseService)
+        {
+            _houseService = houseService;
+        }
 
-		public async Task<IActionResult> Index()
-		{
-			var houses = await _houseService.LastThreeHousesAsync();
+        public async Task<IActionResult> Index()
+        {
+            var houses = await _houseService.LastThreeHousesAsync();
 
-			return View(houses);
-		}
+            return View(houses);
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 400:
+                case 401:
+                    {
+                        return View($"Error{statusCode}");
+                    }
+                default:
+                    {
+                        return View();
+                    }
+            }
+        }
+    }
 }
--- HouseRentingSystem/HouseRentingSystem/Program.cs
+++ HouseRentingSystem/HouseRentingSystem/Program.cs
@@ -10,18 +10,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
-	options.SignIn.RequireConfirmedAccount = false;
-	options.Password.RequireDigit = false;
-	options.Password.RequireLowercase = false;
-	options.Password.RequireUppercase = false;
-	options.Password.RequireNonAlphanumeric = false;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
 })
-	.AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddTransient<IHouseService, HouseService>();
 builder.Services.AddTransient<IAgentService, AgentService>();
@@ -31,13 +31,14 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-	app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
 else
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
