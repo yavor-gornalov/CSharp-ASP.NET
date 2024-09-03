0f7ea210bf77c439db332324f8a2a1b36e4c567c
Yavor Gornalov <y.gornalov@gmail.com>
2024-09-03 10:47:01 +0300

    HouseServiceTests --created

---
+++ HouseRentingSystem/HouseRentingSystem.Tests/UnitTests/AgentServiceTests.cs
@@ -0,0 +1,96 @@
+﻿using HouseRentingSystem.Core.Contracts.Agent;
+using HouseRentingSystem.Core.Services.Agent;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Tests.UnitTests;
+
+public class AgentServiceTests : UnitTestsBase
+{
+    private IAgentService _agentService;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        _agentService = new AgentService(_data);
+    }
+
+    [Test]
+    public async Task CreateAsync_ShouldAddAgentToDatabase()
+    {
+        var userId = "NewAgentUserId";
+        var phoneNumber = "+35922222222";
+
+        await _agentService.CreateAsync(userId, phoneNumber);
+
+        var agentExists = await _data.Agents.AnyAsync(a => a.UserId == userId && a.PhoneNumber == phoneNumber);
+        Assert.That(agentExists, Is.True);
+    }
+
+    [Test]
+    public async Task ExistsByIdAsync_ShouldReturnTrue_IfAgentExists()
+    {
+        var result = await _agentService.ExistsByIdAsync(Agent.UserId);
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public async Task ExistsByIdAsync_ShouldReturnFalse_IfAgentDoesNotExist()
+    {
+        var result = await _agentService.ExistsByIdAsync("NonExistentUserId");
+
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public async Task GetAgentIdAsync_ShouldReturnCorrectId_IfAgentExists()
+    {
+        var resultAgentId = await _agentService.GetAgentIdAsync(Agent.UserId);
+
+        Assert.That(resultAgentId, Is.EqualTo(Agent.Id));
+    }
+
+    [Test]
+    public async Task UserHasRents_ShouldReturnTrue_IfUserHasRents()
+    {
+        var result = await _agentService.UserHasRents(Renter.Id);
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public async Task UserHasRents_ShouldReturnFalse_IfUserHasNoRents()
+    {
+        var nonRenterId = "NonRenterUserId";
+        var nonRenter = new ApplicationUser()
+        {
+            Id = nonRenterId,
+            Email = "nonrenter@mail.bg",
+            FirstName = "Non",
+            LastName = "Renter",
+        };
+        _data.Users.Add(nonRenter);
+        await _data.SaveChangesAsync();
+
+        var result = await _agentService.UserHasRents(nonRenterId);
+
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public async Task UserWithPhonerNumberExistsAsync_ShouldReturnTrue_IfPhoneNumberExists()
+    {
+        var result = await _agentService.UserWithPhonerNumberExistsAsync(Agent.PhoneNumber);
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public async Task UserWithPhonerNumberExistsAsync_ShouldReturnFalse_IfPhoneNumberDoesNotExist()
+    {
+        var result = await _agentService.UserWithPhonerNumberExistsAsync("+35933333333");
+
+        Assert.That(result, Is.False);
+    }
+}
---
+++ HouseRentingSystem/HouseRentingSystem.Tests/UnitTests/HouseServiceTests.cs
@@ -0,0 +1,89 @@
+﻿using HouseRentingSystem.Core.Contracts.House;
+using HouseRentingSystem.Core.Enums;
+using HouseRentingSystem.Core.Models.House;
+using HouseRentingSystem.Core.Services.House;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace HouseRentingSystem.Tests.UnitTests
+{
+    public class HouseServiceTests : UnitTestsBase
+    {
+        private IHouseService _houseService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _houseService = new HouseService(_data);
+        }
+
+        [Test]
+        public async Task ExistAsync_ShouldReturnTrue_IfHouseExists()
+        {
+            var result = await _houseService.ExistAsync(1);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public async Task ExistAsync_ShouldReturnFalse_IfHouseDoesNotExist()
+        {
+            var result = await _houseService.ExistAsync(0);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public async Task CreateAsync_ShouldAddHouseToDatabase()
+        {
+            var model = new HouseFormModel
+            {
+                Title = "New House",
+                Address = "New Address, 123 Str.",
+                Description = "This is a new house description",
+                ImageUrl = "https://example.com/new-house.jpg",
+                PricePerMonth = 2000,
+                CategoryId = 1,
+            };
+
+            var houseId = await _houseService.CreateAsync(model, Agent.Id);
+
+            var houseExists = await _data.Houses.AnyAsync(h => h.Id == houseId);
+            Assert.That(houseExists, Is.True);
+        }
+
+        [Test]
+        public async Task HouseDetailsByIdAsync_ShouldReturnCorrectHouseDetails()
+        {
+            var houseDetails = await _houseService.HouseDetailsByIdAsync(1);
+            Assert.That(houseDetails.Id, Is.EqualTo(1));
+            Assert.That(houseDetails.Title, Is.EqualTo("First Test House"));
+        }
+
+        [Test]
+        public async Task EditAsync_ShouldEditHouseCorrectly()
+        {
+            var houseForEdit = await _data.Houses.FindAsync(2);
+
+            if (houseForEdit == null)
+            {
+                Assert.Fail("House not found");
+            }
+
+            var model = new HouseFormModel
+            {
+                Title = "Second House Edited",
+                Address = houseForEdit.Address,
+                Description = houseForEdit.Description,
+                ImageUrl = houseForEdit.ImageUrl,
+                PricePerMonth = 1500,
+                CategoryId = houseForEdit.CategoryId,
+            };  
+
+            await _houseService.EditAsync(2, model);
+            var updatedHouse = await _data.Houses.FindAsync(2);
+
+            Assert.That(updatedHouse.Title, Is.EqualTo("Second House Edited"));
+            Assert.That(updatedHouse.PricePerMonth, Is.EqualTo(1500));
+        }
+
+    }
+}
